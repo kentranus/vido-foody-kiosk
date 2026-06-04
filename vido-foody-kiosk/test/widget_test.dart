898747c06ddf7c7668651c225038fd2f4eea63a9
kentranus <kentranus@gmail.com>
2026-06-03 21:04:51 -0400

    Delete vido-foody-kiosk directory

--- vido-foody-kiosk/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:vido_foody_kiosk/main.dart';
-
-void main() {
-  testWidgets('Vido Foody Kiosk app loads', (tester) async {
-    await tester.pumpWidget(const VidoFoodyKioskApp());
-    expect(find.text('Welcome to Vido Foody'), findsOneWidget);
-  });
-}
